04d15f55840cb2ba4bf8181803078f5b148de616
Ayu <backyard@ayu.land>
2019-08-20 14:16:23 +0800

    (^^)

--- bmflat.c
+++ bmflat.c
@@ -1,1 +1,9 @@
 #include "bmflat.h"
+
+static char _errmsg[256];
+char *bm_errmsg = _errmsg;
+
+int bm_load(struct bm_chart *chart, const char *source)
+{
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "bmflat.h"
+
+char *read_file(const char *path)
+{
+    FILE *f = fopen(path, "r");
+    if (f == NULL) return NULL;
+
+    char *buf = NULL;
+
+    do {
+        if (fseek(f, 0, SEEK_END) != 0) break;
+        long len = ftell(f);
+        if (fseek(f, 0, SEEK_SET) != 0) break;
+        if ((buf = (char *)malloc(len)) == NULL) break;
+        if (fread(buf, len, 1, f) != 1) { buf = NULL; break; }
+    } while (0);
+
+    if (buf) free(buf);
+    fclose(f);
+    return buf;
+}
 
 int main()
 {
-    puts("Hello, world!");
+    char *src = read_file("sample.bms");
+    if (src == NULL) {
+        puts("> <");
+        return 1;
+    }
+
+    struct bm_chart chart;
+    int line = bm_load(&chart, src);
+    if (line != 0) {
+        printf("Line %d: %s\n", line, bm_errmsg);
+        return 2;
+    }
+
+    puts("(^^)");
+
     return 0;
 }

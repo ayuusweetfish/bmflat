d2f6e9b2e1751510533bf95c920930bc1c11913a
Ayu <backyard@ayu.land>
2019-08-20 14:37:38 +0800

    Add logs

--- bmflat.c
+++ bmflat.c
@@ -1,9 +1,44 @@
 #include "bmflat.h"
 
-static char _errmsg[256];
-char *bm_errmsg = _errmsg;
+#include <stdlib.h>
+#include <string.h>
+
+struct bm_log *bm_logs = NULL;
+static int log_cap, log_ptr;
+
+static void reset_logs()
+{
+    if (bm_logs) free(bm_logs);
+    bm_logs = NULL;
+    log_cap = log_ptr = 0;
+}
+
+static void ensure_log_cap()
+{
+    if (log_cap <= log_ptr) {
+        log_cap = (log_cap == 0 ? 8 : (log_cap << 1));
+        bm_logs = (struct bm_log *)
+            realloc(bm_logs, log_cap * sizeof(struct bm_log));
+    }
+}
+
+#define emit_log(_line, ...) do { \
+    ensure_log_cap(); \
+    bm_logs[log_ptr].line = _line; \
+    snprintf(bm_logs[log_ptr].message, BM_MSG_LEN, __VA_ARGS__); \
+    log_ptr++; \
+} while (0)
 
 int bm_load(struct bm_chart *chart, const char *source)
 {
-    return 0;
+    reset_logs();
+
+    memset(chart, 0, sizeof(struct bm_chart));
+
+    const char *s = "lorem ipsum dolor sit amet";
+    emit_log(0, "heya");
+    emit_log(1, "%s %s %s %s", s, s, s, s);
+    emit_log(2, "%d + %d = %d", 222, 22, 222 + 22);
+
+    return log_ptr;
 }
--- bmflat.h
+++ bmflat.h
@@ -5,10 +5,25 @@
 extern "C" {
 #endif
 
-struct bm_chart {
+struct bm_metadata {
+    char *genre;
+    char *title;
+    char *artist;
+    char *subartist;
 };
 
-extern char *bm_errmsg;
+struct bm_chart {
+    struct bm_metadata meta;
+};
+
+#define BM_MSG_LEN  64
+
+struct bm_log {
+    int line;
+    char message[BM_MSG_LEN];
+};
+
+extern struct bm_log *bm_logs;
 
 int bm_load(struct bm_chart *chart, const char *source);
 

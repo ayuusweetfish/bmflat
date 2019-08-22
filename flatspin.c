f9f303621d58d7f537446b682a9494600af7628e
Ayu <backyard@ayu.land>
2019-08-22 16:43:02 +0800

    Add OpenGL boilerplate code

--- flatspin.c
+++ flatspin.c
@@ -3,8 +3,35 @@
 
 #include <stdio.h>
 
+#define GLSL(__source) "#version 150 core\n" #__source
+
+static inline GLuint load_shader(GLenum type, const char *source)
+{
+    GLuint shader_id = glCreateShader(type);
+    glShaderSource(shader_id, 1, &source, NULL);
+    glCompileShader(shader_id);
+
+    GLint status;
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &status);
+    char msg_buf[1024];
+    glGetShaderInfoLog(shader_id, sizeof(msg_buf) - 1, NULL, msg_buf);
+    fprintf(stderr, "OvO Compilation log for %s shader\n",
+        (type == GL_VERTEX_SHADER ? "vertex" :
+         type == GL_FRAGMENT_SHADER ? "fragment" : "unknown (!)"));
+    fputs(msg_buf, stderr);
+    fprintf(stderr, "=v= End\n");
+    if (status != GL_TRUE) {
+        fprintf(stderr, "> < Shader compilation failed\n");
+        return 0;
+    }
+
+    return shader_id;
+}
+
 int main()
 {
+    // -- Initialization --
+
     if (!glfwInit()) return -1;
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -15,21 +42,80 @@ int main()
 
     GLFWwindow *window = glfwCreateWindow(960, 540, "bmflatspin", NULL, NULL);
     if (window == NULL) {
-        fprintf(stderr, "Cannot initialize GLFW\n");
+        fprintf(stderr, "> < Cannot initialize GLFW\n");
         return -1;
     }
 
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Cannot initialize GLEW\n");
+        fprintf(stderr, "> < Cannot initialize GLEW\n");
         return -1;
     }
+
+    // -- Resource allocation --
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    float v[][5] = {
+        {0.0f, 0.5f, 1.0f, 1.0f, 0.9f},
+        {0.5f, -0.5f, 1.0f, 0.9f, 1.0f},
+        {-0.5f, -0.5f, 1.0f, 0.4f, 0.2f}
+    };
+    glBufferData(GL_ARRAY_BUFFER, sizeof v, v, GL_STREAM_DRAW);
+
+    const char *vshader_source = GLSL(
+        in vec2 ppp;
+        in vec3 qwq;
+        out vec3 qwq_frag;
+        void main()
+        {
+            gl_Position = vec4(ppp, 0.0, 1.0);
+            qwq_frag = qwq;
+        }
+    );
+    GLuint vshader = load_shader(GL_VERTEX_SHADER, vshader_source);
+
+    const char *fshader_source = GLSL(
+        in vec3 qwq_frag;
+        out vec4 ooo;
+        void main()
+        {
+            ooo = vec4(qwq_frag, 1.0f);
+        }
+    );
+    GLuint fshader = load_shader(GL_FRAGMENT_SHADER, fshader_source);
+
+    GLuint prog = glCreateProgram();
+    glAttachShader(prog, vshader);
+    glAttachShader(prog, fshader);
+    glBindFragDataLocation(prog, 0, "ooo");
+    glLinkProgram(prog);
+    glUseProgram(prog);
+
+    GLuint ppp_attrib_index = glGetAttribLocation(prog, "ppp");
+    glEnableVertexAttribArray(ppp_attrib_index);
+    glVertexAttribPointer(ppp_attrib_index, 2, GL_FLOAT, GL_FALSE,
+        5 * sizeof(float), (void *)0);
+
+    GLuint qwq_attrib_index = glGetAttribLocation(prog, "qwq");
+    glEnableVertexAttribArray(qwq_attrib_index);
+    glVertexAttribPointer(qwq_attrib_index, 2, GL_FLOAT, GL_FALSE,
+        5 * sizeof(float), (void *)(2 * sizeof(float)));
+
+    // -- Event/render loop --
 
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.15f, 0.1f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }

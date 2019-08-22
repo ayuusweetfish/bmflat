0ff37d47e3c91c90ba452c5936958b88758a5415
Shiqing <shiqing-thu18@yandex.com>
2019-08-22 16:16:16 +0800

    Add GLEW

--- flatspin.c
+++ flatspin.c
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <stdio.h>
@@ -6,13 +7,25 @@ int main()
 {
     if (!glfwInit()) return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
     GLFWwindow *window = glfwCreateWindow(960, 540, "bmflatspin", NULL, NULL);
     if (window == NULL) {
-        glfwTerminate();
+        fprintf(stderr, "Cannot initialize GLFW\n");
         return -1;
     }
 
     glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Cannot initialize GLEW\n");
+        return -1;
+    }
 
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.15f, 0.1f, 0.3f, 1.0f);
